29c7fa1c5949487f2cf3434bf4649f7f29051398
fadelBayloun <fadel.bayloun12@gmail.com>
2023-11-16 23:27:05 +0200

    added the carousel section and fixed an error in the programs section

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// Programs Section
+
 const cards = document.querySelectorAll(".program-card");
 const btns = document.querySelectorAll(".programs-li");
 const container = document.querySelector(".programs-container");
@@ -32,4 +34,27 @@ function changeProgram(id, color) {
 
   activeBtn = id;
   activeProgram = cardId;
+}
+
+// Testimonials Section
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
